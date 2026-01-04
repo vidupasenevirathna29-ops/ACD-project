51faaecf50fbc8b9295bfee93abc4f3e63dcfaff
Vidupa Senevirathna <vidupasenevirathna29@gmail.com>
2026-01-05 08:56:02 +1200

    Enhanced property search form component

--- estate-agent-spa/src/components/SearchForm.jsx
+++ estate-agent-spa/src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./SearchForm.css";
 
 function SearchForm({ onSearch }) {
   const [type, setType] = useState("any");
@@ -25,12 +26,12 @@ function SearchForm({ onSearch }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+    <form onSubmit={handleSubmit} className="search-form">
       <h3>Search Filters</h3>
 
-      <div>
+      <div className="form-group">
         <label>
-          Type:{" "}
+          Type:
           <select value={type} onChange={(e) => setType(e.target.value)}>
             <option value="any">Any</option>
             <option value="House">House</option>
@@ -39,69 +40,75 @@ function SearchForm({ onSearch }) {
         </label>
       </div>
 
-      <div>
-        <label>
-          Min Price:{" "}
-          <input
-            type="number"
-            value={minPrice}
-            onChange={(e) => setMinPrice(e.target.value)}
-          />
-        </label>
+      <div className="form-group">
+        <div className="form-row">
+          <label>
+            Min Price:
+            <input
+              type="number"
+              value={minPrice}
+              onChange={(e) => setMinPrice(e.target.value)}
+            />
+          </label>
 
-        <label style={{ marginLeft: "10px" }}>
-          Max Price:{" "}
-          <input
-            type="number"
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(e.target.value)}
-          />
-        </label>
+          <label>
+            Max Price:
+            <input
+              type="number"
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+          </label>
+        </div>
       </div>
 
-      <div>
-        <label>
-          Min Beds:{" "}
-          <input
-            type="number"
-            value={minBeds}
-            onChange={(e) => setMinBeds(e.target.value)}
-          />
-        </label>
+      <div className="form-group">
+        <div className="form-row">
+          <label>
+            Min Beds:
+            <input
+              type="number"
+              value={minBeds}
+              onChange={(e) => setMinBeds(e.target.value)}
+            />
+          </label>
 
-        <label style={{ marginLeft: "10px" }}>
-          Max Beds:{" "}
-          <input
-            type="number"
-            value={maxBeds}
-            onChange={(e) => setMaxBeds(e.target.value)}
-          />
-        </label>
+          <label>
+            Max Beds:
+            <input
+              type="number"
+              value={maxBeds}
+              onChange={(e) => setMaxBeds(e.target.value)}
+            />
+          </label>
+        </div>
       </div>
 
-      <div>
-        <label>
-          Date added from:{" "}
-          <input
-            type="date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
-        </label>
+      <div className="form-group">
+        <div className="form-row">
+          <label>
+            Date added from:
+            <input
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </label>
 
-        <label style={{ marginLeft: "10px" }}>
-          to:{" "}
-          <input
-            type="date"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-          />
-        </label>
+          <label>
+            to:
+            <input
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </label>
+        </div>
       </div>
 
-      <div>
+      <div className="form-group">
         <label>
-          Postcode area:{" "}
+          Postcode area:
           <input
             type="text"
             value={postcode}
@@ -111,7 +118,7 @@ function SearchForm({ onSearch }) {
         </label>
       </div>
 
-      <button type="submit" style={{ marginTop: "10px" }}>
+      <button type="submit" className="search-button">
         Search
       </button>
     </form>
